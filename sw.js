a6e4661e5e8bc5e25e2dee1fc61b358bb6a59f64
LucasDevGuedes <lucassantosguedessouza@gmail.com>
2025-11-09 10:52:18 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,45 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("quiz-cache-v1").then(cache => cache.addAll([
-      "./",
-      "./index.html",
-      "./manifest.json",
-      "./icons/icon-192.png",
-      "./icons/icon-512.png"
-    ]))
+const CACHE_NAME = 'quiz-cristao-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => {
+        console.log('Erro ao cachear:', err);
+      })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
